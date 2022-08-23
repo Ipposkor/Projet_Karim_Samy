57ba593bea300c7e1beacd03254d13dc3d0aefec
Karim <Karim.krayem@gmail.com>
2022-08-23 14:03:43 +0200

    first commit with comps

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Switch } from 'react-router-dom';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Favorite from './component/Favorite';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Search />
+
+      <Switch>
+        <Route path={"/"}>
+
+        </Route>
+        <Route path={"/Favoris"}>
+          <Favorite />
+        </Route>
+
+      </Switch>
 
     </div>
   );
---
+++ src/component/Favorite.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import axios from 'axios'
+
+
+
+import React from 'react'
+
+const Favorite = () => {
+    return (
+        <div>Favorite</div>
+    )
+}
+
+export default Favorite
