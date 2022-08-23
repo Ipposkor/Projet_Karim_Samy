bc27bc777f6a503e2ff7d5bae8e49a0d86abb118
ippo <ippo@hotmail.com>
2022-08-23 15:58:44 +0200

    api et debut des cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { Route, Switch,Link } from 'react-router-dom';
+import axios from 'axios';
+import React,{useState, useEffect} from 'react';
 import './App.css';
+import Card from './component/card/Card';
 
 function App() {
+  const [movies,setMovies] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=1d3ead8417a1330e1bfbf4195f564f62&language=en-US&page=1').then((response)=>{
+      setMovies(response.data.results)
+    }).catch(err=>{console.log(err)})
+  },[])
+
   return (
     <div className="App">
+      <Link to={"/"}>
+        <button>HOME</button>
+      </Link>
+      <Switch>
+        
+        <Route path={"/Favoris"}>
 
+        </Route>
+        <Route path={"/"}>
+          <Link to={"/Favoris"}>
+            <span>fav</span>
+          </Link>
+          {movies.map((item,index)=>{
+            return <Card key={index} item={item} />
+          })}
+        </Route>
+      </Switch>
     </div>
   );
 }
