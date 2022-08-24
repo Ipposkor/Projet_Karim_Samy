698b2c69f5b337bc216e03443a288fadff24f3b8
Karim <Karim.krayem@gmail.com>
2022-08-24 11:32:36 +0200

    changed style of fav page and added search bar

--- src/App.js
+++ src/App.js
@@ -1,58 +1,62 @@
 
+import React, { useState, useEffect } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import Favorite from './component/Favorite';
-import Search from './component/Search';
-
-import { Route, Switch,Link } from 'react-router-dom';
-import axios from 'axios';
-import React,{useState, useEffect} from 'react';
-
 import './App.css';
+import Favorite from './component/Favoris/Favorite';
+import Search from './component/Favoris/Search';
 import Card from './component/card/Card';
 
 function App() {
-  const [movies,setMovies] = useState([])
+  const [movies, setMovies] = useState([])
+  const [fav, setFav] = useState('')
+  const [filteredInput, setFilteredInput] = useState('');
+  const inputChangeHandler = (selectedCountry) => {
+    setFilteredInput(selectedCountry);
+    console.log('agg');
+  };
 
-  useEffect(()=>{
-  axios.get('https://api.themoviedb.org/2/movie/top_rated?api_key=1d3ead8417a1330e1bfbf4195f564f62&language=en-US&page=1').then((response)=>{
+  const test2 = (e) => {
+    setFav((preve) => {
+      return [...preve, e]
+    })
+  }
+
+  useEffect(() => {
+    axios.get('https://api.jikan.moe/v3/search/anime?q=onepiece').then((response) => {
       setMovies(response.data.results)
-    }).catch(err=>{console.log(err)})
-  },[])
+      console.log(response)
+    }).catch(err => { console.log(err) })
+  }, [])
+
+  console.log(fav);
 
   return (
     <div className="App">
-
-      <Search />
-
       <Switch>
-
-
         <Route path={"/Favoris"}>
-
-          <Favorite />
-
-        </Route>
-      </Switch>
-
-      <Link to={"/"}>
-        <button>HOME</button>
-      </Link>
-		<Switch>
-        
-        <Route path={"/Favoris"}>
-
-
+          <Search onchangeInput={inputChangeHandler} />
+          <Link to={"/"}>
+            <button>HOME</button>
+          </Link>
+          {movies.map((item, index) => {
+            if (item.title.toLowerCase().includes(filteredInput.toLowerCase())) {
+              return (
+                <div>
+                  <Favorite item={item} key={index} />
+                </div>)
+            }
+          })}
         </Route>
         <Route path={"/"}>
           <Link to={"/Favoris"}>
             <span>fav</span>
           </Link>
-          {movies.map((item,index)=>{
+          {movies.map((item, index) => {
             return <Card key={index} item={item} />
           })}
         </Route>
+
       </Switch>
     </div>
   );
--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,14 +1,13 @@
 import { Link } from 'react-router-dom';
 import './Card.css'
 
-function Card(props){
-    function getID(e){
-        props.
+function Card(props) {
+    function getID(e) {
         console.log(e.target.id);
     }
-    return(
+    return (
         <div>
-            <img className='img' src={`https://www.themoviedb.org/t/p/w440_and_h660_face/${props.item.poster_path}`} alt="" />
+            <img className='img' src={props.item.image_url} alt="" />
             <h3 className='title'>{props.item.title}</h3>
             <div className='overview'>{props.item.overview}</div>
             <span className='date'>{props.item.release_date}</span>
